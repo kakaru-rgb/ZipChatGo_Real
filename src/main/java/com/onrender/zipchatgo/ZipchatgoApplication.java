4d5b44c3cb8c6399c787ab100864b5b6fa93d848
kakaru-rgb <aodwnsdud2001@gmail.com>
2026-08-24 14:13:55 +0900

    first commit

---
+++ src/main/java/com/onrender/zipchatgo/ZipchatgoApplication.java
@@ -0,0 +1,13 @@
+package com.onrender.zipchatgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipchatgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipchatgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onrender/zipchatgo/ZipchatgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onrender.zipchatgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipchatgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
