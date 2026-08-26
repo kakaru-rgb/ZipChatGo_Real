72c07f860e715e7f756887e91dd5e70a81b8d63d
hsohsh1111-sys <hsohsh1111@gmail.com>
2026-08-26 13:05:11 +0900

    111
    11

--- src/main/java/com/onrender/zipchatgo/support/SupportPageController.java
+++ src/main/java/com/onrender/zipchatgo/support/SupportPageController.java
@@ -1,10 +1,10 @@
 package com.onrender.zipchatgo.support;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/support")
